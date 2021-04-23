288cad0419b8b12af5a8d5bb0d70127bc576f306
Tyler Doerksen <tylerd@microsoft.com>
2021-04-23 10:03:10 -0500

    Add project files.

---
+++ VS2017-NET472-Web-App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VS2017_NET472_Web_App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ VS2017-NET472-Web-App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2017_NET472_Web_App.MvcApplication" Language="C#" %>
